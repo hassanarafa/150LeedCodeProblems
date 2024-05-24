1297135fb4dc990ec63f4750b24e8bab9afe995d
hassanarafa <hassan.arafa.e@gmail.com>
2024-05-24 15:49:43 +0300

    ContainsDuplicate

--- src/main/java/org/example/ContainsDuplicate.java
+++ src/main/java/org/example/ContainsDuplicate.java
@@ -1,6 +1,20 @@
 package org.example;
-public class ContainsDuplicate {
-    public static void main(String[] args) {
 
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++)
+            if (nums[i] == nums[i + 1])
+                return true;
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        int[] x = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(x));
     }
 }

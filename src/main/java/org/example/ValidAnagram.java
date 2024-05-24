44490c780a967a22f288bbda8fda522743c26551
hassanarafa <hassan.arafa.e@gmail.com>
2024-05-24 16:01:55 +0300

    ValidAnagram

---
+++ src/main/java/org/example/ValidAnagram.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        char[]x =s.toCharArray();
+        char[]y =t.toCharArray();
+        Arrays.sort(x);
+        Arrays.sort(y);
+        return Arrays.equals(x,y);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram","nagaram"));
+    }
+}
